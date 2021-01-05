90812ca5edf537dd15a7558c9796c7de131fe2b8
iOS <ios@iOS-Coaching.local>
2021-01-05 15:52:08 +0530

    Added test cases

---
+++ VasuCommonCode/VasuCommonCode.h
@@ -0,0 +1,18 @@
+//
+//  VasuCommonCode.h
+//  VasuCommonCode
+//
+//  Created by iOS on 05/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VasuCommonCode.
+FOUNDATION_EXPORT double VasuCommonCodeVersionNumber;
+
+//! Project version string for VasuCommonCode.
+FOUNDATION_EXPORT const unsigned char VasuCommonCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VasuCommonCode/PublicHeader.h>
+
+
---
+++ VasuCommonCode/VasuCommonCode.swift
@@ -0,0 +1,20 @@
+//
+//  VasuCommonCode.swift
+//  VasuCommonCode
+//
+//  Created by iOS on 05/01/21.
+//
+
+public final class VasuCommonCode {
+
+    let name = "VasuCommonCode"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
